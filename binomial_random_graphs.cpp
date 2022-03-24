dd76c34e29d233465c5a3368a3bf677054d10bde
aantunezc <100408523+aantunezc@users.noreply.github.com>
2022-03-24 19:37:38 +0100

    Update binomial_random_graphs.cpp

--- binomial_random_graphs.cpp
+++ binomial_random_graphs.cpp
@@ -27,7 +27,8 @@ typedef boost::graph_traits<UndirectedGraph>::adjacency_iterator adjacency_itera
 
 vector<bool> visited;
 vector<int> cc;
-int n = 1000;
+int n = 100;
+vector<int> cicles_cc;
 /* -------------------------------------------------- */
 
 /* ------------ FUNCIONS I PROCESSOS ---------------- */
@@ -84,14 +85,6 @@ vector<int> getVertices(const UndirectedGraph& g){
     return vertices;
 }
 
-bool noCcLeft(){
-    bool noneLeft = true;
-    for (const int& i : cc) {
-        noneLeft = i!=0;
-    }
-    return noneLeft;
-}
-
 void dfsCc(const UndirectedGraph& g, const int& v,
            const int& father, const int& nCc){
     visited[v] = true;
@@ -101,25 +94,71 @@ void dfsCc(const UndirectedGraph& g, const int& v,
     for (adjacency_iterator it = ai; it != ai_end; ++it){
         int w = *it;
         if (!visited[w]) dfsCc(g,w,v,nCc);
+        else if(father  != w) ++cicles_cc[nCc];
     }
 }
 
-void getCc(const UndirectedGraph& g){
+int getCc(const UndirectedGraph& g){
      // Inicialitzem el vector de vertexos visitats
     visited = vector<bool>(n,false); // Inicialitzem el vector de components conexes
     cc = vector<int>(n,0); // Inicialitzem el vector de components conexes a 0
     vector<int> vertices = getVertices(g); //
-    int nCc = 1; // Primera component conexa
+    int nCc = 0; // Primera component conexa
+    cicles_cc.push_back(-1);
     int v;
     for (int i = 0; i < vertices.size(); ++i){
         v = vertices[i];
         if (!visited[v]) {
-            dfsCc(g,v,v,nCc);
             ++nCc;
+            cicles_cc.push_back(0);
+            dfsCc(g,v,v,nCc);
+            
         };
     }
     
-    printCc();
+    //printCc();
+    return nCc;
+}
+
+void printCCC(){
+    for (int i = 1; i < cicles_cc.size(); ++i){
+        if(cicles_cc[i] <= 1) cout << "La component conexa " << i << " es complexa" << endl;
+        else cout << "La component conexa " << i << " no es complexa" << endl;
+    }
+}
+
+
+UndirectedGraph vertex_percolation(const UndirectedGraph& g, const double p){
+    UndirectedGraph g_per = g;
+    vector<int> vertices = getVertices(g_per);
+    for (int i = 0; i < vertices.size(); ++i){
+        double p1 = (double)(rand()) / ((double)RAND_MAX);
+        if (p1 <= p) boost::remove_vertex (i, g_per);
+    }
+    return g_per;
+
+}
+
+
+UndirectedGraph edge_percolation(const UndirectedGraph& g, const double p){
+    UndirectedGraph g_per = g;
+    vector<int> vertices = getVertices(g_per);
+    int v;
+    for (int i = 0; i < vertices.size(); ++i){
+        v = vertices[i];
+        adjacency_iterator ai, ai_end;
+        boost::tie(ai, ai_end) = boost::adjacent_vertices(v,g);
+        for (adjacency_iterator it = ai; it != ai_end; ++it){
+            double p1 = (double)(rand()) / ((double)RAND_MAX);
+            if (p1 <= p){
+                int w = *it;
+                boost::remove_edge (v, w, g_per);
+            }
+        }
+            
+    }
+    return g_per;
+
 }
 /* -------------------------------------------------- */
 
@@ -127,15 +166,22 @@ int main(){
     // Definició del graf
     UndirectedGraph g(n);
     // Addició les arestes del graf
-    double p = 0.03;
+    double p = 0.01;
     addEdges(g, p);
     // Recorregut per les arestes del graf
     printEdges(g);
     // Recorregut pels vertexos del graf
     printVertices(g);
 
-    getCc(g);
+    int ncc = getCc(g);
+    printCCC();
 
-    cout << "Number of edges = " << boost::num_edges(g) << endl;
+    UndirectedGraph g_eper = edge_percolation(g, 0.05);
+    // Recorregut per les arestes del graf
+    printEdges(g_eper);
+    // Recorregut pels vertexos del graf
+    printVertices(g_eper);
+    
+
     return 0;
 }
